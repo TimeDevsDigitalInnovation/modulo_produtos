1e5888ec4dbdd06fb4d7bb4c82b0f29700041638
Emmanuel de Oliveira D'Abruzzo <emmanuel.oliveira@trtsp.jus.br>
2021-12-16 22:54:43 -0300

    Kickoff

---
+++ src/main/java/com/timedevsdigitalinnovation/ecommerce/catalogo_produtos/CatalogoProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.timedevsdigitalinnovation.ecommerce.catalogo_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timedevsdigitalinnovation/ecommerce/catalogo_produtos/CatalogoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timedevsdigitalinnovation.ecommerce.catalogo_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
